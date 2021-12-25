37f8ee0e8435027e3f9fc968ca4433635db322d8
hannis chou <hannis505@gmail.com>
2021-12-25 20:55:36 +0800

    refactor product model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -79,7 +79,6 @@ class UsersController < ApplicationController
                 :msg=>"fail"
             }
         end
-
     end
 
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,19 +2,11 @@ class Product < ApplicationRecord
     belongs_to:user
     has_many:cart
 
-    def get_info
-        return {id: self[:id],name: self[:name],price: self[:price],quentity: self[:quentity]}
-    end
 
     def self.get_products_by_userId(id)
         products=Product.where(user_id: id)
-        products_info=[]
 
-        products.each do |product|
-            products_info<<product.get_info
-        end
-
-        return products_info
+        return products.map {|p| p.as_json(:except=>[:created_at,:updated_at])}
     end
 
     
